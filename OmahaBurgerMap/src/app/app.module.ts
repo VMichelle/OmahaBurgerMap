fe586e2fcfa56c6a46c6d047148a8c379ce6556f
Michelle Vu <th.michellevu@gmail.com>
2019-04-30 09:07:38 -0500

    initial setup

---
+++ OmahaBurgerMap/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MapComponent } from './Components/map/map.component';
+import { RightColComponent } from './components/right-col/right-col.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MapComponent,
+    RightColComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
